22e71b5c5c03ab1196f9b8921b0144e922bbbb37
Ben Brandt <benjamin.j.brandt@gmail.com>
2024-12-02 23:35:34 +0100

    Day 1: blinking light + setup

---
+++ src/bin/day1_blink.rs
@@ -0,0 +1,26 @@
+//! This example tests the RP Pico onboard LED.
+
+#![no_std]
+#![no_main]
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after_secs(1).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}
